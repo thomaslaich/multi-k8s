e4ce25a5b2e6421576d0d165ab2dd1a0081e4e02
Thomas Laich <thomaslaich@gmail.com>
2022-07-24 01:14:45 +0200

    change title

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
+          <h1>Fib calculator kubernetes</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
